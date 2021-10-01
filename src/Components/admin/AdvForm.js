f42f5b71ecf2b969d547fb6e0c73ad21be598758
frandrew90 <frandrewjs@gmail.com>
2021-10-01 15:58:50 +0300

    fixed

--- src/Components/admin/AdvForm.js
+++ src/Components/admin/AdvForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { AdvFormContainer } from './AdvFormStyled';
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 
 const productCategories = ['phones', 'laptops'];
 
